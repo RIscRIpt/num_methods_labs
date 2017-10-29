4b26767977920ed94affe32202661ff72682aa50
RIscRIpt <richard@riscript.com>
2017-10-29 20:42:50 +0200

    lab2: add 'both' plotter plot.R

---
+++ lab2/plot.R
@@ -0,0 +1,46 @@
+#!/usr/bin/env Rscript
+
+library(polynom)
+args <- commandArgs(trailingOnly=TRUE)
+
+outputFile   <- args[1]
+inputPolynom <- args[2]
+inputSpline  <- args[3]
+
+png(outputFile)
+
+splines <- invisible(read.table(file(inputSpline, "r")))
+stopifnot(length(splines) == 5)
+colnames(splines) <- c("x", "a", "b", "c", "d")
+
+xs <- splines[, "x"]
+ys <- splines[, "a"]
+
+xlim   <- c(xs[1], xs[length(xs)])
+ylim   <- c(min(ys), max(ys))
+xamp   <- xlim[2] - xlim[1]
+yamp   <- ylim[2] - ylim[1]
+liminc <- 0.1
+xlim   <- c(xlim[1] - xamp * liminc, xlim[2] + xamp * liminc)
+ylim   <- c(ylim[1] - yamp * liminc, ylim[2] + yamp * liminc)
+
+plot(0, type='n', axes=TRUE, ann=FALSE, xlim=xlim, ylim=ylim)
+grid()
+points(xs, ys, col="red", pch=19)
+
+for (i in 1:(nrow(splines) - 1)) {
+    x_left <- splines[i, "x"]
+    x_right <- splines[i + 1, "x"]
+    a <- splines[i, "a"]
+    b <- splines[i, "b"]
+    c <- splines[i, "c"]
+    d <- splines[i, "d"]
+    spline <- function(x) { a + b * (x - x_left) + c * (x - x_left)^2 + d * (x - x_left)^3 }
+    curve(spline, from=x_left, to=x_right, col="blue", add=TRUE)
+}
+
+poly <- as.function(polynomial(scan(file(inputPolynom, "r"), quiet=TRUE)))
+curve(poly, from=xlim[1], to=xlim[2], col="green", add=TRUE)
+
+invisible(dev.off())
+
