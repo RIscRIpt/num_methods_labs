f045c7d16f58eae073ac0383bd6382c8ea9f0ce8
RIscRIpt <richard@riscript.com>
2017-10-29 22:26:27 +0200

    lab2: spline.R fix plot 1 point

--- lab2/spline.R
+++ lab2/spline.R
@@ -18,15 +18,17 @@ png(outputFile)
 plot(0, type='n', axes=TRUE, ann=FALSE, xlim=xlim, ylim=ylim)
 points(xs, ys, col="red", pch=19)
 
-for (i in 1:(nrow(splines) - 1)) {
-    x_left <- splines[i, "x"]
-    x_right <- splines[i + 1, "x"]
-    a <- splines[i, "a"]
-    b <- splines[i, "b"]
-    c <- splines[i, "c"]
-    d <- splines[i, "d"]
-    spline <- function(x) { a + b * (x - x_left) + c * (x - x_left)^2 + d * (x - x_left)^3 }
-    curve(spline, from=x_left, to=x_right, col="blue", add=TRUE)
+if (nrow(splines) > 1) {
+    for (i in seq(1, nrow(splines) - 1, 1)) {
+        x_left <- splines[i, "x"]
+        x_right <- splines[i + 1, "x"]
+        a <- splines[i, "a"]
+        b <- splines[i, "b"]
+        c <- splines[i, "c"]
+        d <- splines[i, "d"]
+        spline <- function(x) { a + b * (x - x_left) + c * (x - x_left)^2 + d * (x - x_left)^3 }
+        curve(spline, from=x_left, to=x_right, col="blue", add=TRUE)
+    }
 }
 
 invisible(dev.off())
