6b55524607a4a06415ab2f7f2962f0cfc0f29c04
RIscRIpt <richard@riscript.com>
2017-12-03 13:29:35 +0200

    lab3: add plotter

---
+++ lab3/plot.R
@@ -0,0 +1,27 @@
+#!/usr/bin/env Rscript
+
+pow <- function(b, e) {
+    return(b^e)
+}
+
+args <- commandArgs(trailingOnly=TRUE)
+
+func <- eval(parse(text=paste("function(x) { return(", args[1], ") }")))
+left <- eval(parse(text=args[2]))
+right <- eval(parse(text=args[3]))
+output <- args[4]
+if (is.na(output) || output == '') {
+    output <- "plot.png"
+}
+
+stdin <- file("stdin", "r")
+roots <- scan(stdin, quiet=TRUE)
+
+png(output, width=900, height=600)
+
+curve(func, left, right, ylab=args[1])
+points(roots, func(roots), pch=19, col="red")
+abline(h=0, col="gray")
+abline(v=roots, col="gray")
+invisible(dev.off())
+
